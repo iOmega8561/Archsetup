b235deed50781cdabcc1f48e65c5dd74cb268981
iOmega8561 <giusepperocco38@gmail.com>
2022-09-11 13:07:47 +0200

    renamed

--- amd_zen_ext4.sh
+++ amd_zen_ext4.sh
@@ -1,12 +1,24 @@
-echo "Please mount partitions first:"
-echo "ROOT part to /mnt"
-echo "BOOT part to /mnt/boot"
-echo "HOME part to /mnt/home"
-echo "PRESS ENTER TO INITIATE SETUP"
-read
+MSG="\e[33mARCHSCRIPT\e[0m:"
 
 ############################################################################
-# PACSTRAP
+# CONF VARS
+
+LANG="it_IT.UTF-8"
+KEYMAP="it"
+TIMEZONE="Europe/Rome"
+LINUXOPT="nvidia-drm.modeset=1"
+
+############################################################################
+# INIT
+
+echo "$MSG Please mount partitions first:"
+echo "ROOT part to /mnt"
+echo "BOOT part to /mnt/boot"
+echo "HOME part to /mnt/home (if present)"
+echo "$MSG PRESS ENTER TO INITIATE SETUP"
+read
+
+echo "$MSG Executing pacstrap to /mnt"
 
 pacstrap /mnt base linux-zen linux-firmware amd-ucode base-devel sudo networkmanager nano python git
 sleep 3
@@ -14,21 +26,21 @@ sleep 3
 ############################################################################
 # FSTAB
 
-echo "Generating fstab"
+echo "$MSG Generating fstab"
 genfstab -U /mnt > /mnt/etc/fstab
 sleep 3
 
 ############################################################################
 # SYSTEMD-BOOT INSTALLATION
 
-echo "Installing systemd-boot to /mnt/boot"
+echo "$MSG Installing systemd-boot to /mnt/boot"
 arch-chroot /mnt bootctl install
 sleep 3
 
 ############################################################################
 # SYSTEMD-BOOT CONFIGURATION
 
-echo "Generating bootloader configuration"
+echo "$MSG Generating bootloader configuration"
 tee /mnt/boot/loader/loader.conf <<- EOF >> /dev/null
 	default normal
 	timeout 3
@@ -39,42 +51,42 @@ sleep 3
 ############################################################################
 # SYSTEMD-BOOT ENTRIES
 
-echo "Generating bootloader entries"
+echo "$MSG Generating bootloader entries"
 
-STR=$(mount | grep " on /mnt type ext4 (rw,relatime)")
-ROOT=${STR%" on /mnt type ext4 (rw,relatime)"}
+MOUNT=$(mount | grep " on /mnt type ext4 (rw,relatime)")
+ROOT=${MOUNT%" on /mnt type ext4 (rw,relatime)"}
 
 tee /mnt/boot/loader/entries/fallback.conf <<- EOF >> /dev/null
 	title "Arch Linux (fallback initramfs)"
 	linux /vmlinuz-linux-zen
 	initrd /amd-ucode.img
 	initrd /initramfs-linux-zen-fallback.img
-	options root=$ROOT rw nvidia-drm.modeset=1
+	options root=$ROOT rw $LINUXOPT
 EOF
 tee /mnt/boot/loader/entries/normal.conf <<- EOF >> /dev/null
 	title "Arch Linux"
 	linux /vmlinuz-linux-zen
 	initrd /amd-ucode.img
 	initrd /initramfs-linux-zen.img
-	options root=$ROOT rw nvidia-drm.modeset=1
+	options root=$ROOT rw $LINUXOPT
 EOF
 sleep 3
 
 ############################################################################
 # LOCALES
 
-echo "Setting up locales for it_IT.UTF-8"
+echo "$MSG Setting up locale $LANG"
 
 tee -a /mnt/etc/locale.gen <<- EOF >> /dev/null
-	it_IT.UTF-8 UTF-8
+	$LANG UTF-8
 EOF
 
 tee /mnt/etc/locale.conf <<- EOF >> /dev/null
-	LANG=it_IT.UTF-8
+	LANG=$LANG
 EOF
 
 tee /mnt/etc/vconsole.conf <<- EOF >> /dev/null
-	KEYMAP=it
+	KEYMAP=$KEYMAP
 EOF
 
 arch-chroot /mnt locale-gen
@@ -83,10 +95,10 @@ sleep 3
 ############################################################################
 # USER CREATION
 
-read -p "Enter a valid user name: " NAME
+read -p "$MSG Enter a valid user name: " NAME
 arch-chroot /mnt useradd $NAME -m
 
-printf "Enter a valid password"
+printf "$MSG Enter a valid password || "
 arch-chroot /mnt passwd $NAME
 
 mkdir -p /mnt/etc/sudoers.d
@@ -98,12 +110,14 @@ sleep 3
 ############################################################################
 # TIME AND TIMEZONE
 
-ln -sf /mnt/usr/share/zoneinfo/Europe/Rome /etc/localtime
+echo "$MSG Setting timezone and system clock"
+
+ln -sf /mnt/usr/share/zoneinfo/$TIMEZONE /etc/localtime
 arch-chroot /mnt hwclock --systohc
 sleep 3
 
 ############################################################################
 # TERMINATING
 
-echo "SETUP PROCESS COMPLETED"
+echo "$MSG SETUP PROCESS COMPLETED"
 sleep 2
